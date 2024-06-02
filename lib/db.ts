7c24143f8757c99b445b6630e7083155072ceb37
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-02 05:42:58 +0000

    local and prod

--- lib/db.ts
+++ lib/db.ts
@@ -2,20 +2,15 @@ import { createClient } from "@libsql/client";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
-// export const db =
-//     NODE_ENV === "production"
-//         ? createClient({
-//               url: process.env.TURSO_DATABASE_URL!,
-//               authToken: process.env.TURSO_AUTH_TOKEN!,
-//           })
-//         : createClient({
-//               url: "file:local.db",
-//           });
-
-export const db = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-});
+export const db =
+    NODE_ENV === "production"
+        ? createClient({
+              url: process.env.TURSO_DATABASE_URL!,
+              authToken: process.env.TURSO_AUTH_TOKEN!,
+          })
+        : createClient({
+              url: "file:local.db",
+          });
 
 export interface DatabaseUser {
     id: string;
