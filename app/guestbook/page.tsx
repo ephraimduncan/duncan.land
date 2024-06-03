c0a2fe7104503883ef6377b767d5c3ecf671610b
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-06-03 21:04:42 +0000

    fix: add loading

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,15 +1,8 @@
-import { Button } from "@/components/button";
-import { auth } from "@/lib/auth";
-
-import { SignDialog } from "./sign-dialog";
-import { logout } from "@/lib/actions/logout";
 import { db } from "@/lib/db";
 import { Card } from "@/components/card";
 import Image from "next/image";
-import { MotionDiv } from "@/components/motion";
-import { GithubIcon } from "@/components/ui/GithubIcon";
-import { SignOutIcon } from "@/components/ui/SignoutIcon";
-import { Skeleton } from "@/components/skeleton";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type PostsQuery = {
     id: string;
@@ -20,105 +13,48 @@ type PostsQuery = {
     name: string;
 };
 
-const CardSkeleton = () => {
-    return (
-        <Card className="rounded-lg flex flex-col justify-between space-y-3 h-32 col-span-12 sm:col-span-6">
-            <Skeleton className="w-full h-12" />
-        </Card>
-    );
-};
-
 export default async function GuestbookPage() {
-    const { user } = await auth();
     const postsQuery = await db.execute(
         "SELECT post.*, user.username, user.name FROM post JOIN user ON post.user_id = user.id ORDER BY post.created_at DESC"
     );
     const posts = postsQuery.rows as unknown as PostsQuery[];
 
-    const variant = {
-        hidden: { opacity: 0, y: -5 },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.2 } },
-    };
-
-    const cardVariants = {
-        hidden: { opacity: 0, y: -5, transition: { delay: 0.2 } },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.2 } },
-    };
-
     return (
-        <section>
-            <MotionDiv initial="hidden" animate="visible" variants={variant} className="space-y-4">
-                <h1 className="font-medium text-2xl tracking-tighter">Sign my guestbook</h1>
+        <Suspense fallback={<Loading />}>
+            <ul className="grid grid-cols-12 gap-5 mt-10">
+                {posts.map((post) => (
+                    <li key={post.id} className="flex col-span-12 sm:col-span-6">
+                        <Card className="rounded-lg flex flex-col justify-between space-y-3 h-full">
+                            <p className="leading-6 text-grey-900 dark:text-grey-50">{post.message}</p>
 
-                {user ? (
-                    <>
-                        <div className="flex w-full justify-between items-center">
-                            <SignDialog user={user} />
-                            <form action={logout}>
-                                <Button plain type="submit">
-                                    <SignOutIcon />
-                                    Sign out
-                                </Button>
-                            </form>
-                        </div>
-                    </>
-                ) : (
-                    <Button href="/login/github" color="light">
-                        <GithubIcon />
-                        Sign in with GitHub
-                    </Button>
-                )}
-            </MotionDiv>
-
-            <MotionDiv initial="hidden" animate="visible" variants={cardVariants} className="space-y-4">
-                {!posts.length && (
-                    <div className="grid grid-cols-12 gap-5 mt-10">
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                        <CardSkeleton />
-                    </div>
-                )}
-
-                <ul className="grid grid-cols-12 gap-5 mt-10">
-                    {posts.map((post) => (
-                        <li key={post.id} className="flex col-span-12 sm:col-span-6">
-                            <Card className="rounded-lg flex flex-col justify-between space-y-3 h-full">
-                                <p className="leading-6 text-grey-900 dark:text-grey-50">{post.message}</p>
-
-                                <div className="mt-auto flex items-center justify-between">
-                                    <div className="flex flex-col justify-end h-full text-sm">
-                                        {post.name ? (
-                                            <p className="font-bold">{post.name}</p>
-                                        ) : (
-                                            <p className="font-bold">@{post.username}</p>
-                                        )}
-
-                                        <p>
-                                            {new Date(post.created_at * 1000).toLocaleString("en-US", {
-                                                year: "numeric",
-                                                month: "short",
-                                                day: "numeric",
-                                                hour: "numeric",
-                                                minute: "numeric",
-                                            })}
-                                        </p>
-                                    </div>
-                                    {post.signature && (
-                                        <div className="dark:invert -mb-4 -mr-4">
-                                            <Image alt="signature" src={post.signature} width={150} height={150} />
-                                        </div>
+                            <div className="mt-auto flex items-center justify-between">
+                                <div className="flex flex-col justify-end h-full text-sm">
+                                    {post.name ? (
+                                        <p className="font-bold">{post.name}</p>
+                                    ) : (
+                                        <p className="font-bold">@{post.username}</p>
                                     )}
+
+                                    <p>
+                                        {new Date(post.created_at * 1000).toLocaleString("en-US", {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "numeric",
+                                            hour: "numeric",
+                                            minute: "numeric",
+                                        })}
+                                    </p>
                                 </div>
-                            </Card>
-                        </li>
-                    ))}
-                </ul>
-            </MotionDiv>
-        </section>
+                                {post.signature && (
+                                    <div className="dark:invert -mb-4 -mr-4">
+                                        <Image alt="signature" src={post.signature} width={150} height={150} />
+                                    </div>
+                                )}
+                            </div>
+                        </Card>
+                    </li>
+                ))}
+            </ul>
+        </Suspense>
     );
 }
