53992637d335e7cea126653b28a41bb1e1b9c198
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-11-22 14:14:51 +0000

    new theme selector

--- components/signature-pad/signature-pad.tsx
+++ components/signature-pad/signature-pad.tsx
@@ -1,272 +1,293 @@
 "use client";
 
-import type { HTMLAttributes, MouseEvent, PointerEvent, TouchEvent } from "react";
-import { useEffect, useMemo, useRef, useState } from "react";
-import type { ClassValue } from "clsx";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { unsafe_useEffectOnce } from "@/lib/hooks/use-effect-once";
+import { cn } from "@/lib/utils";
 import type { StrokeOptions } from "perfect-freehand";
 import { getStroke } from "perfect-freehand";
-import { unsafe_useEffectOnce } from "@/lib/hooks/use-effect-once";
+import type {
+  HTMLAttributes,
+  MouseEvent,
+  PointerEvent,
+  TouchEvent,
+} from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { getSvgPathFromStroke } from "./helper";
 import { Point } from "./point";
 
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
 const DPI = 2;
 
-export type SignaturePadProps = Omit<HTMLAttributes<HTMLCanvasElement>, "onChange"> & {
-    onChange?: (_signatureDataUrl: string | null) => void;
-    containerClassName?: string;
-    disabled?: boolean;
+export type SignaturePadProps = Omit<
+  HTMLAttributes<HTMLCanvasElement>,
+  "onChange"
+> & {
+  onChange?: (_signatureDataUrl: string | null) => void;
+  containerClassName?: string;
+  disabled?: boolean;
 };
 
 export const SignaturePad = ({
-    className,
-    containerClassName,
-    defaultValue,
-    onChange,
-    disabled = false,
-    ...props
+  className,
+  containerClassName,
+  defaultValue,
+  onChange,
+  disabled = false,
+  ...props
 }: SignaturePadProps) => {
-    const $el = useRef<HTMLCanvasElement>(null);
-    const $imageData = useRef<ImageData | null>(null);
+  const $el = useRef<HTMLCanvasElement>(null);
+  const $imageData = useRef<ImageData | null>(null);
 
-    const [isPressed, setIsPressed] = useState(false);
-    const [lines, setLines] = useState<Point[][]>([]);
-    const [currentLine, setCurrentLine] = useState<Point[]>([]);
+  const [isPressed, setIsPressed] = useState(false);
+  const [lines, setLines] = useState<Point[][]>([]);
+  const [currentLine, setCurrentLine] = useState<Point[]>([]);
 
-    const perfectFreehandOptions = useMemo(() => {
-        const size = $el.current ? Math.min($el.current.height, $el.current.width) * 0.03 : 10;
+  const perfectFreehandOptions = useMemo(() => {
+    const size = $el.current
+      ? Math.min($el.current.height, $el.current.width) * 0.03
+      : 10;
 
-        return {
-            size,
-            thinning: 0.25,
-            streamline: 0.5,
-            smoothing: 0.5,
-            end: {
-                taper: size * 2,
-            },
-        } satisfies StrokeOptions;
-    }, []);
+    return {
+      size,
+      thinning: 0.25,
+      streamline: 0.5,
+      smoothing: 0.5,
+      end: {
+        taper: size * 2,
+      },
+    } satisfies StrokeOptions;
+  }, []);
 
-    const onMouseDown = (event: MouseEvent | PointerEvent | TouchEvent) => {
-        if (event.cancelable) {
-            event.preventDefault();
+  const onMouseDown = (event: MouseEvent | PointerEvent | TouchEvent) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
+
+    setIsPressed(true);
+
+    const point = Point.fromEvent(event, DPI, $el.current);
+
+    setCurrentLine([point]);
+  };
+
+  const onMouseMove = (event: MouseEvent | PointerEvent | TouchEvent) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
+
+    if (!isPressed) {
+      return;
+    }
+
+    const point = Point.fromEvent(event, DPI, $el.current);
+
+    if (point.distanceTo(currentLine[currentLine.length - 1]) > 5) {
+      setCurrentLine([...currentLine, point]);
+
+      // Update the canvas here to draw the lines
+      if ($el.current) {
+        const ctx = $el.current.getContext("2d");
+
+        if (ctx) {
+          ctx.restore();
+          ctx.imageSmoothingEnabled = true;
+          ctx.imageSmoothingQuality = "high";
+
+          lines.forEach((line) => {
+            const pathData = new Path2D(
+              getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)),
+            );
+
+            ctx.fill(pathData);
+          });
+
+          const pathData = new Path2D(
+            getSvgPathFromStroke(
+              getStroke([...currentLine, point], perfectFreehandOptions),
+            ),
+          );
+          ctx.fill(pathData);
         }
+      }
+    }
+  };
 
-        setIsPressed(true);
+  const onMouseUp = (
+    event: MouseEvent | PointerEvent | TouchEvent,
+    addLine = true,
+  ) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
 
-        const point = Point.fromEvent(event, DPI, $el.current);
+    setIsPressed(false);
 
-        setCurrentLine([point]);
-    };
+    const point = Point.fromEvent(event, DPI, $el.current);
 
-    const onMouseMove = (event: MouseEvent | PointerEvent | TouchEvent) => {
-        if (event.cancelable) {
-            event.preventDefault();
-        }
+    const newLines = [...lines];
 
-        if (!isPressed) {
-            return;
-        }
+    if (addLine && currentLine.length > 0) {
+      newLines.push([...currentLine, point]);
+      setCurrentLine([]);
+    }
 
-        const point = Point.fromEvent(event, DPI, $el.current);
+    setLines(newLines);
 
-        if (point.distanceTo(currentLine[currentLine.length - 1]) > 5) {
-            setCurrentLine([...currentLine, point]);
+    if ($el.current && newLines.length > 0) {
+      const ctx = $el.current.getContext("2d");
 
-            // Update the canvas here to draw the lines
-            if ($el.current) {
-                const ctx = $el.current.getContext("2d");
+      if (ctx) {
+        ctx.restore();
 
-                if (ctx) {
-                    ctx.restore();
-                    ctx.imageSmoothingEnabled = true;
-                    ctx.imageSmoothingQuality = "high";
+        ctx.imageSmoothingEnabled = true;
+        ctx.imageSmoothingQuality = "high";
 
-                    lines.forEach((line) => {
-                        const pathData = new Path2D(getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)));
+        newLines.forEach((line) => {
+          const pathData = new Path2D(
+            getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)),
+          );
+          ctx.fill(pathData);
+        });
 
-                        ctx.fill(pathData);
-                    });
+        onChange?.($el.current.toDataURL());
+        ctx.save();
+      }
+    }
+  };
 
-                    const pathData = new Path2D(
-                        getSvgPathFromStroke(getStroke([...currentLine, point], perfectFreehandOptions))
-                    );
-                    ctx.fill(pathData);
-                }
-            }
-        }
-    };
+  const onMouseEnter = (event: MouseEvent | PointerEvent | TouchEvent) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
 
-    const onMouseUp = (event: MouseEvent | PointerEvent | TouchEvent, addLine = true) => {
-        if (event.cancelable) {
-            event.preventDefault();
-        }
+    if ("buttons" in event && event.buttons === 1) {
+      onMouseDown(event);
+    }
+  };
 
-        setIsPressed(false);
+  const onMouseLeave = (event: MouseEvent | PointerEvent | TouchEvent) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
 
-        const point = Point.fromEvent(event, DPI, $el.current);
+    onMouseUp(event, false);
+  };
 
-        const newLines = [...lines];
+  const onClearClick = () => {
+    if ($el.current) {
+      const ctx = $el.current.getContext("2d");
 
-        if (addLine && currentLine.length > 0) {
-            newLines.push([...currentLine, point]);
-            setCurrentLine([]);
-        }
+      ctx?.clearRect(0, 0, $el.current.width, $el.current.height);
+      $imageData.current = null;
+    }
 
-        setLines(newLines);
+    onChange?.(null);
 
-        if ($el.current && newLines.length > 0) {
-            const ctx = $el.current.getContext("2d");
+    setLines([]);
+    setCurrentLine([]);
+  };
 
-            if (ctx) {
-                ctx.restore();
+  const onUndoClick = () => {
+    if (lines.length === 0) {
+      return;
+    }
 
-                ctx.imageSmoothingEnabled = true;
-                ctx.imageSmoothingQuality = "high";
+    const newLines = lines.slice(0, -1);
+    setLines(newLines);
 
-                newLines.forEach((line) => {
-                    const pathData = new Path2D(getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)));
-                    ctx.fill(pathData);
-                });
+    // Clear the canvas
+    if ($el.current) {
+      const ctx = $el.current.getContext("2d");
+      const { width, height } = $el.current;
+      ctx?.clearRect(0, 0, width, height);
 
-                onChange?.($el.current.toDataURL());
-                ctx.save();
-            }
-        }
-    };
+      if (typeof defaultValue === "string" && $imageData.current) {
+        ctx?.putImageData($imageData.current, 0, 0);
+      }
 
-    const onMouseEnter = (event: MouseEvent | PointerEvent | TouchEvent) => {
-        if (event.cancelable) {
-            event.preventDefault();
-        }
+      newLines.forEach((line) => {
+        const pathData = new Path2D(
+          getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)),
+        );
+        ctx?.fill(pathData);
+      });
 
-        if ("buttons" in event && event.buttons === 1) {
-            onMouseDown(event);
-        }
-    };
+      onChange?.($el.current.toDataURL());
+    }
+  };
 
-    const onMouseLeave = (event: MouseEvent | PointerEvent | TouchEvent) => {
-        if (event.cancelable) {
-            event.preventDefault();
-        }
+  useEffect(() => {
+    if ($el.current) {
+      $el.current.width = $el.current.clientWidth * DPI;
+      $el.current.height = $el.current.clientHeight * DPI;
+    }
+  }, []);
 
-        onMouseUp(event, false);
-    };
+  unsafe_useEffectOnce(() => {
+    if ($el.current && typeof defaultValue === "string") {
+      const ctx = $el.current.getContext("2d");
 
-    const onClearClick = () => {
-        if ($el.current) {
-            const ctx = $el.current.getContext("2d");
+      const { width, height } = $el.current;
 
-            ctx?.clearRect(0, 0, $el.current.width, $el.current.height);
-            $imageData.current = null;
-        }
+      const img = new Image();
 
-        onChange?.(null);
+      img.onload = () => {
+        ctx?.drawImage(
+          img,
+          0,
+          0,
+          Math.min(width, img.width),
+          Math.min(height, img.height),
+        );
 
-        setLines([]);
-        setCurrentLine([]);
-    };
+        const defaultImageData = ctx?.getImageData(0, 0, width, height) || null;
 
-    const onUndoClick = () => {
-        if (lines.length === 0) {
-            return;
-        }
+        $imageData.current = defaultImageData;
+      };
 
-        const newLines = lines.slice(0, -1);
-        setLines(newLines);
+      img.src = defaultValue;
+    }
+  });
 
-        // Clear the canvas
-        if ($el.current) {
-            const ctx = $el.current.getContext("2d");
-            const { width, height } = $el.current;
-            ctx?.clearRect(0, 0, width, height);
+  return (
+    <div
+      className={cn("relative block", containerClassName, {
+        "pointer-events-none opacity-50": disabled,
+      })}
+    >
+      <canvas
+        ref={$el}
+        className={cn("relative block dark:invert", className)}
+        style={{ touchAction: "none" }}
+        onPointerMove={(event) => onMouseMove(event)}
+        onPointerDown={(event) => onMouseDown(event)}
+        onPointerUp={(event) => onMouseUp(event)}
+        onPointerLeave={(event) => onMouseLeave(event)}
+        onPointerEnter={(event) => onMouseEnter(event)}
+        {...props}
+      />
 
-            if (typeof defaultValue === "string" && $imageData.current) {
-                ctx?.putImageData($imageData.current, 0, 0);
-            }
-
-            newLines.forEach((line) => {
-                const pathData = new Path2D(getSvgPathFromStroke(getStroke(line, perfectFreehandOptions)));
-                ctx?.fill(pathData);
-            });
-
-            onChange?.($el.current.toDataURL());
-        }
-    };
-
-    useEffect(() => {
-        if ($el.current) {
-            $el.current.width = $el.current.clientWidth * DPI;
-            $el.current.height = $el.current.clientHeight * DPI;
-        }
-    }, []);
-
-    unsafe_useEffectOnce(() => {
-        if ($el.current && typeof defaultValue === "string") {
-            const ctx = $el.current.getContext("2d");
-
-            const { width, height } = $el.current;
-
-            const img = new Image();
-
-            img.onload = () => {
-                ctx?.drawImage(img, 0, 0, Math.min(width, img.width), Math.min(height, img.height));
-
-                const defaultImageData = ctx?.getImageData(0, 0, width, height) || null;
-
-                $imageData.current = defaultImageData;
-            };
-
-            img.src = defaultValue;
-        }
-    });
-
-    return (
-        <div
-            className={cn("relative block", containerClassName, {
-                "pointer-events-none opacity-50": disabled,
-            })}
+      <div className="absolute bottom-4 right-4 flex gap-2">
+        <button
+          type="button"
+          className="focus-visible:ring-ring ring-offset-background text-muted-foreground/60 hover:text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
+          onClick={() => onClearClick()}
         >
-            <canvas
-                ref={$el}
-                className={cn("relative block dark:invert", className)}
-                style={{ touchAction: "none" }}
-                onPointerMove={(event) => onMouseMove(event)}
-                onPointerDown={(event) => onMouseDown(event)}
-                onPointerUp={(event) => onMouseUp(event)}
-                onPointerLeave={(event) => onMouseLeave(event)}
-                onPointerEnter={(event) => onMouseEnter(event)}
-                {...props}
-            />
+          Clear Signature
+        </button>
+      </div>
 
-            <div className="absolute bottom-4 right-4 flex gap-2">
-                <button
-                    type="button"
-                    className="focus-visible:ring-ring ring-offset-background text-muted-foreground/60 hover:text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
-                    onClick={() => onClearClick()}
-                >
-                    Clear Signature
-                </button>
-            </div>
-
-            {lines.length > 0 && (
-                <div className="absolute bottom-4 left-4 flex gap-2">
-                    <button
-                        type="button"
-                        title="undo"
-                        className="focus-visible:ring-ring ring-offset-background text-muted-foreground/60 hover:text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
-                        onClick={() => onUndoClick()}
-                    >
-                        Undo
-                        <span className="sr-only">Undo</span>
-                    </button>
-                </div>
-            )}
+      {lines.length > 0 && (
+        <div className="absolute bottom-4 left-4 flex gap-2">
+          <button
+            type="button"
+            title="undo"
+            className="focus-visible:ring-ring ring-offset-background text-muted-foreground/60 hover:text-muted-foreground rounded-full p-0 text-xs focus-visible:outline-none focus-visible:ring-2"
+            onClick={() => onUndoClick()}
+          >
+            Undo
+            <span className="sr-only">Undo</span>
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
