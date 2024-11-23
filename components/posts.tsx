9e2862de1395f81f9b26fea9b1e7d9eb97c9ad42
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-11-23 02:17:39 +0000

    feat: better transitions

---
+++ components/posts.tsx
@@ -0,0 +1,65 @@
+import { allPosts } from "@/.contentlayer/generated";
+import { allThoughts } from "@/.contentlayer/generated/index.mjs";
+import { formatter } from "@/lib/utils";
+
+import { Link as NextViewTransition } from "next-view-transitions";
+import React from "react";
+
+interface PostProps {
+  category: "thoughts" | "posts" | "archive";
+}
+
+const getPosts = (category: "thoughts" | "posts" | "archive") => {
+  if (category === "thoughts") {
+    return allThoughts;
+  } else if (category === "archive") {
+    return allPosts.filter((post) => post.archived);
+  } else {
+    return allPosts.filter((post) => !post.archived);
+  }
+};
+
+export const Posts = ({ category }: PostProps) => {
+  const posts = getPosts(category).sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+
+  const Seperator = () => (
+    <div className="border-border border-t border-grey-900" />
+  );
+
+  if (posts.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <NextViewTransition
+        href={`/${category}`}
+        className="flex justify-between"
+      >
+        <h2 className="py-2 text-grey-100">
+          {category === "posts" ? "blog" : category}{" "}
+          {posts.length > 0 && `(${posts.length})`}
+        </h2>
+      </NextViewTransition>
+
+      {posts.map((post) => {
+        return (
+          <React.Fragment key={post.slug}>
+            <Seperator />
+            <NextViewTransition
+              href={post.slug}
+              className="flex w-full justify-between py-2 text-grey-100"
+            >
+              <p>{post.title}</p>
+              <p className="mt-0 text-grey-100">
+                {formatter.date(new Date(post.date))}
+              </p>
+            </NextViewTransition>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const date = (input: Date): string => {
+  return new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  }).format(input);
+};
+
+const number = (input: number): string => {
+  return new Intl.NumberFormat("en", {
+    style: "decimal",
+  }).format(input);
+};
+
+const time = (value: number, unit: Intl.RelativeTimeFormatUnit): string => {
+  return new Intl.RelativeTimeFormat("en", {
+    numeric: "auto",
+  }).format(value, unit);
+};
+
+export const formatter = {
+  date,
+  number,
+  time,
+};
