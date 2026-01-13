dadede71fd86a792079e96640e2db2214c2b0387
ephraimduncan <ephraimduncan68@gmail.com>
2026-01-13 12:32:32 +0000

    chore: add better auth url

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,8 @@ import { drizzleDb } from "./drizzle";
 import * as schema from "./schema";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGINS?.split(",") || [],
   database: drizzleAdapter(drizzleDb, {
     provider: "sqlite",
     schema,
